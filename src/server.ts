e18599a3086de68abe1cde7a3a5169bc1ad94e79
kakimaru <memimei0306@gmail.com>
2024-10-30 08:33:38 +0900

    I tried exercises

---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import http from "http";
+
+const server = http.createServer(
+  (req: http.IncomingMessage, res: http.ServerResponse) => {
+    if (req.url === "/") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("<h1>Home</h1>");
+      res.end();
+      return;
+    }
+
+    if (req.url === "/about") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write("<h1>About</h1>");
+      res.end();
+      return;
+    }
+
+    if (req.url === "/my-account") {
+      res.writeHead(403, { "Content-Type": "text/plain" });
+      res.write("You have no access to this page.");
+      res.end();
+      return;
+    }
+
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.write("Page not found.");
+    res.end();
+  }
+);
+
+const PORT: number = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
